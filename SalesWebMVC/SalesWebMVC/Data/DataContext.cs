4f92b3337beabe34bf7c2c5dbff03e74165cd035
GabrielInacio03 <inaciogs03@gmail.com>
2025-02-17 05:38:51 -0300

    CRUD Scaffolding

---
+++ SalesWebMVC/SalesWebMVC/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext (DbContextOptions<DataContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMVC.Models.Department> Department { get; set; } = default!;
+    }
+}
